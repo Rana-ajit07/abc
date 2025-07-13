f9134a836e4b2f918188a7be763ad3f94a66b1d3
Rana-ajit07 <singhajitr023@gmail.com>
2025-07-13 11:58:41 +0530

    first commit

---
+++ src/main/java/com/thetesting/App.java
@@ -0,0 +1,13 @@
+package com.thetesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
